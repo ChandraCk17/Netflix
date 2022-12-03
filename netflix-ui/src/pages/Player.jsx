03c3467edddd552542f184b56bd3fd124a7e91e7
ChandraCk17 <mohitckdubey@gmail.com>
2022-12-03 17:55:48 +0530

    video autoplay

--- netflix-ui/src/pages/Player.jsx
+++ netflix-ui/src/pages/Player.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import styled from "styled-components";
 import { BsArrowLeft } from "react-icons/bs";
 import { useNavigate } from "react-router-dom";
-import video from "../assets/videoplayback.webm";
+import video from "../assets/video.mp4";
+
 export default function Player() {
   const navigate = useNavigate();
 
